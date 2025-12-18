293def2e8ce6351971af0eb65c568c48459e03e9
Darkxx14 <packs.gg1@gmail.com>
2025-12-18 12:37:07 +0530

    remove commented code

--- paper/src/main/java/com/xyrisdev/mist/module/format/stage/FormatStage.java
+++ paper/src/main/java/com/xyrisdev/mist/module/format/stage/FormatStage.java
@@ -5,10 +5,6 @@ import com.xyrisdev.mist.api.processor.ChatStage;
 import com.xyrisdev.mist.hook.LuckPermsHook;
 import com.xyrisdev.mist.module.format.config.FormatConfiguration;
 import com.xyrisdev.mist.module.format.entry.FormatEntry;
-import com.xyrisdev.mist.util.text.MistTextParser;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.event.HoverEvent;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.model.user.User;
 import org.bukkit.entity.Player;
@@ -21,50 +17,6 @@ public final class FormatStage implements ChatStage {
 	public FormatStage(@NotNull FormatConfiguration config) {
 		this.config = config;
 	}
-
-//	@Override
-//	@SuppressWarnings("deprecation")
-//	public void process(@NotNull ChatContext context) {
-//		final Player player = context.player();
-//
-//		final String group = group(player);
-//		final FormatEntry entry = config.resolve(group);
-//
-//		if (entry == null) {
-//			return;
-//		}
-//
-//		final String raw = entry.message().replace("<message>", context.plain());
-//
-//		Component component = MistTextParser.parse(player, raw);
-//
-//		// hover
-//		if (!entry.hoverText().isEmpty()) {
-//			Component hover = Component.empty();
-//
-//			for (String line : entry.hoverText()) {
-//				hover = hover.append(
-//						MistTextParser.parse(player, line)
-//				).append(Component.newline());
-//			}
-//
-//			component = component.hoverEvent(
-//					HoverEvent.showText(hover)
-//			);
-//		}
-//
-//		// click
-//		if (entry.action() != null) {
-//			component = component.clickEvent(
-//					ClickEvent.clickEvent(
-//							entry.action().action(),
-//							entry.action().value()
-//					)
-//			);
-//		}
-//
-//		context.message(component);
-//	}
 
 	@Override
 	public void process(@NotNull ChatContext context) {
