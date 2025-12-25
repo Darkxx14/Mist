e5c87307bbe7d6835f30ca87fba9de5c2c62348d
Darkxx14 <packs.gg1@gmail.com>
2025-12-25 14:46:58 +0530

    refactor: aesthetics

---
+++ paper/src/main/java/com/xyrisdev/mist/util/text/tags/ColorTag.java
@@ -0,0 +1,78 @@
+package com.xyrisdev.mist.util.text.tags;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.minimessage.tag.Modifying;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class ColorTag implements Modifying {
+
+	// palette from github.com/nulli0n/nightcore-spigot
+	private static final Map<String, TextColor> PALETTE = Map.ofEntries(
+			color("m_black", "#000000"),
+			color("m_white", "#ffffff"),
+			color("m_gray", "#aaa8a8"),
+			color("m_green", "#74ea31"),
+			color("m_yellow", "#ead931"),
+			color("m_orange", "#ea9631"),
+			color("m_red", "#ea3131"),
+			color("m_blue", "#3196ea"),
+			color("m_cyan", "#31eace"),
+			color("m_purple", "#bd31ea"),
+			color("m_pink", "#ea31b2"),
+
+			color("m_light_gray", "#d4d9d8"),
+			color("m_light_green", "#91f251"),
+			color("m_light_yellow", "#ffeea2"),
+			color("m_light_orange", "#fdba5e"),
+			color("m_light_red", "#fd5e5e"),
+			color("m_light_blue", "#5e9dfd"),
+			color("m_light_cyan", "#5edefd"),
+			color("m_light_purple", "#e39fff"),
+			color("m_light_pink", "#fd8ddb"),
+
+			color("m_dark_gray", "#6c6c62")
+	);
+
+	private final TextColor color;
+
+	private ColorTag(@NotNull TextColor color) {
+		this.color = color;
+	}
+
+	@Override
+	public @NotNull Component apply(@NotNull Component current, int depth) {
+		if (depth != 0) {
+			return Component.empty();
+		}
+
+		return current.color(this.color);
+	}
+
+	public static @NotNull TagResolver resolver() {
+		final TagResolver.Builder builder = TagResolver.builder();
+
+		PALETTE.forEach((name, color) ->
+				builder.tag(name, (args, ctx) -> new ColorTag(color))
+		);
+
+		return builder.build();
+	}
+
+	@Contract("_, _ -> new")
+	private static Map.@NotNull @Unmodifiable Entry<String, TextColor> color(@NotNull String name, @NotNull String hex) {
+		return Map.entry(name, requireColor(hex, name));
+	}
+
+	private static @NotNull TextColor requireColor(@NotNull String hex, @NotNull String name) {
+		return Objects.requireNonNull(TextColor.fromHexString(hex),
+				() -> "Invalid hex color '" + hex + "' for tag '" + name + "'"
+		);
+	}
+}
--- paper/src/main/java/com/xyrisdev/mist/util/text/tags/registry/TagRegistry.java
+++ paper/src/main/java/com/xyrisdev/mist/util/text/tags/registry/TagRegistry.java
@@ -1,5 +1,6 @@
 package com.xyrisdev.mist.util.text.tags.registry;
 
+import com.xyrisdev.mist.util.text.tags.ColorTag;
 import com.xyrisdev.mist.util.text.tags.PlaceholderAPITag;
 import com.xyrisdev.mist.util.text.tags.SmallCapsTag;
 import net.kyori.adventure.audience.Audience;
@@ -9,6 +10,7 @@ import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
@@ -16,19 +18,19 @@ import java.util.function.Function;
 public class TagRegistry {
 
 	private final Map<Set<String>, Function<Audience, ? extends Tag>> tags = new LinkedHashMap<>();
-	private final Map<Set<String>, Function<Audience, TagResolver>> resolvers = new LinkedHashMap<>();
+	private final Set<Function<Audience, TagResolver>> resolvers = new LinkedHashSet<>();
 
 	public TagRegistry() {
-		register(Set.of("smallcaps", "sc"), audience -> new SmallCapsTag());
-		registerResolver(Set.of("papi", "placeholderapi"), PlaceholderAPITag::create);
+		register(Set.of("smallcaps"), audience -> new SmallCapsTag());
+		registerResolver(PlaceholderAPITag::create);
 	}
 
 	public void register(@NotNull Set<String> names, @NotNull Function<Audience, ? extends Tag> factory) {
 		this.tags.put(names, factory);
 	}
 
-	public void registerResolver(@NotNull Set<String> names, @NotNull Function<Audience, TagResolver> factory) {
-		this.resolvers.put(names, factory);
+	public void registerResolver(@NotNull Function<Audience, TagResolver> factory) {
+		this.resolvers.add(factory);
 	}
 
 	public @NotNull TagResolver build(@NotNull Audience audience) {
@@ -38,11 +40,12 @@ public class TagRegistry {
 				builder.tag(names, (args, ctx) -> factory.apply(audience))
 		);
 
-		this.resolvers.forEach((names, factory) ->
+		this.resolvers.forEach(factory ->
 				builder.resolver(factory.apply(audience))
 		);
 
 		builder.resolvers(StandardTags.defaults());
+		builder.resolver(ColorTag.resolver());
 		return builder.build();
 	}
 }
