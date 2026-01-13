8defda70c0d6c10cccd14669222c6d42d56f2764
Darkxx14 <packs.gg1@gmail.com>
2026-01-13 14:59:57 +0530

    a

--- paper/src/main/java/com/xyrisdev/mist/command/MistCallbackCommand.java
+++ paper/src/main/java/com/xyrisdev/mist/command/MistCallbackCommand.java
@@ -3,18 +3,18 @@ package com.xyrisdev.mist.command;
 import com.xyrisdev.mist.Mist;
 import com.xyrisdev.mist.extension.render.inventory.impl.InventoryRenderService;
 import org.bukkit.entity.Player;
-import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
 import org.incendo.cloud.paper.util.sender.PlayerSource;
-import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NullMarked;
 
 import java.util.UUID;
 
+@NullMarked
 @SuppressWarnings("unused")
 public class MistCallbackCommand {
 
 	@Command("mistcallback <id>")
-	public void callback(@NotNull PlayerSource sender, @Argument("id") UUID id) {
+	public void callback(PlayerSource sender, UUID id) {
 		final Player player = sender.source();
 
 		Mist.INSTANCE.scheduler()
