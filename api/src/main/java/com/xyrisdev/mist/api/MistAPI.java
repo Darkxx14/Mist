4abc2368fdb1a7c1fc5e6f664278dfe5407c66ae
Darkxx14 <packs.gg1@gmail.com>
2026-01-15 15:37:12 +0530

    fix: fix javadoc

--- api/src/main/java/com/xyrisdev/mist/api/MistAPI.java
+++ api/src/main/java/com/xyrisdev/mist/api/MistAPI.java
@@ -1,6 +1,7 @@
 package com.xyrisdev.mist.api;
 
 import com.xyrisdev.mist.api.chat.extension.registry.MistExtensionRegistry;
+import com.xyrisdev.mist.api.event.MistEvent;
 import com.xyrisdev.mist.api.event.MistEventBus;
 import com.xyrisdev.mist.api.event.internal.impl.MistEventBusImpl;
 import org.jetbrains.annotations.NotNull;
@@ -28,13 +29,22 @@ public final class MistAPI {
 	 * <p>The extension registry allows registering and executing
 	 * {@code @MistExtension}-annotated handlers during chat processing.</p>
 	 *
-	 * @return the global chat extension registry
+	 * @return the chat extension registry
 	 * @since 1.0.0
 	 */
 	public static @NotNull MistExtensionRegistry extensions() {
 		return EXTENSIONS;
 	}
 
+	/**
+	 * Returns the event bus used by Mist.
+	 *
+	 * <p>The event bus is responsible for publishing and dispatching
+	 * {@link MistEvent}s to registered listeners.</p>
+	 *
+	 * @return the Mist event bus
+	 * @since 1.0.0
+	 */
 	public static @NotNull MistEventBus eventBus() {
 		return EVENTBUS;
 	}
