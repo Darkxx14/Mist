d94ae92aa1417c20cf335ac08a4d239bd1a433cb
Darkxx14 <packs.gg1@gmail.com>
2026-01-30 12:03:53 +0530

    a

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 rootProject.name = "Mist"
 
 include(
@@ -12,8 +16,4 @@ pluginManagement {
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
     }
-}
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
